165aa6ecd6161767e335eccaa0fef15bce189c75
Nehal Shams <nehalshams786@gmail.com>
2021-12-25 01:02:15 +0530

    user info project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [user, setUser] = useState([]);
+
+
+    useEffect(() => {
+        fetch("https://reqres.in/api/users?page=2").then((result) =>{
+        result.json().then((resp) =>{
+                setUser(resp.data);
+                console.log(user);
+        });
+    });
+    },[]);
+    return(
+        <>
+        <h1>User Information</h1>
+            <div className="App">
+
+                <table id="user">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Picture</th>
+                    </tr>
+                    {
+                        user.map((item) =>
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.first_name} {item.last_name}</td>
+                            <td>{item.email}</td>
+                            <td><img src={item.avatar} alt="" /></td>
+                        </tr>
+                        )
+                    }
+
+                </table>
+            </div>
+        </>
+    )
 }
 
 export default App;
